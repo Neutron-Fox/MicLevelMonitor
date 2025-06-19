acdc29fe41f3632994bfe0d252d393037e1299f8
Neutron-Fox <vajdazolazoli@gmail.com>
2025-06-19 10:17:41 +0200

    Minden is megvan

--- obj/Release/net8.0-windows/win-x64/MicLevelMonitor.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/win-x64/MicLevelMonitor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicLevelMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e45608196359111d86ed778c6b334579e90e452")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ed551b85399f24edb010414d87e43b5db6df6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicLevelMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicLevelMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

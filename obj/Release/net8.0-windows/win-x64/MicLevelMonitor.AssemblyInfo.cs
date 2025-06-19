77dc522d97feade056efac431771b46392faaefa
Neutron-Fox <vajdazolazoli@gmail.com>
2025-06-19 12:27:23 +0200

    sergaaaaaaaaa

--- obj/Release/net8.0-windows/win-x64/MicLevelMonitor.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/win-x64/MicLevelMonitor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicLevelMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0209b7a8b06ac6e8bea920599740a62fa9e0b633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c76b770ff52a0649c15dc58cbd57efa1b1224b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicLevelMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicLevelMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

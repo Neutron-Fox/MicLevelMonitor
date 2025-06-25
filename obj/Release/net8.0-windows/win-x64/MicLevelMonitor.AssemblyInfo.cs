ece10bcf7088d2325d591bcf750a4bc7c707fb56
Neutron-Fox <vajdazolazoli@gmail.com>
2025-06-25 12:02:26 +0200

    eaf

--- obj/Release/net8.0-windows/win-x64/MicLevelMonitor.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/win-x64/MicLevelMonitor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicLevelMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1a0a8b47fa924c4922b3462532b8b8702956315")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8c8507551a745b7a42bdf8ebc4b9ed4553bd578")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicLevelMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicLevelMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
